2f2cd481aefb73f18249eb3e7ab31bfc2173049f
Akhand <akhndprtp@gmail.com>
2025-08-18 16:16:09 +0530

    client deployment fix

--- server/server.js
+++ server/server.js
@@ -11,7 +11,10 @@ dotenv.config({ path: './config.env' });
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: process.env.CLIENT_URL,
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
